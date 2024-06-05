abb405e7e1a2a1070d92a719caf3f65859c5e518
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-06-05 18:39:30 +0530

    create pages folder and add dependency

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Toaster } from "react-hot-toast";
+
+const App = () => {
+  return <Router>
+    <Route path="/" element={} />
+    <Route path="/" element={} />
+    <Route path="/" element={} />
+    <Route path="/" element={} />
+  </Router>
+}
+
+export default App
