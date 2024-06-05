9978fffabf02c1975823e472758f8da2c4bab17b
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-06-05 16:02:30 +0530

    Updated reservationRouter File

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,9 +1,9 @@
 import exppress from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { dbConnection } from "./database/dbConnection.js"
-import { errorMiddleware } from "./error/error.js"
-import reservationRouter from "./routes/reservationRoute.js"
+import { dbConnection } from "./database/dbConnection.js";
+import { errorMiddleware } from "./error/error.js";
+import reservationRouter from "./routes/reservationRoute.js";
 
 
 const app = exppress()
@@ -14,10 +14,8 @@ app.use(cors({
     methods: ["POST"],
     credentials: true,
 }));
-
 app.use(exppress.json());
 app.use(exppress.urlencoded({ extended: true }));
-
 app.use('/api/v1/reservation', reservationRouter)
 
 dbConnection();
