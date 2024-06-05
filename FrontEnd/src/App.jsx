b349562fa0f61d49a02dd88d742db03ef5674411
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-06-06 01:50:39 +0530

    Add Herosection.jsx file

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Toaster } from "react-hot-toast";
-import Home from "../Pages/Home";
-import NotFound from "../Pages/NotFound";
-import Success from "../Pages/Success";
+import Home from "./Pages/Home";
+import Success from "./Pages/Success";
+import NotFound from "./Pages/NotFound";
 
 const App = () => {
   return( 
