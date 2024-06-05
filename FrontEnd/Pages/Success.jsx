b71950a159c66b71363e8db593d10f2c8aa7e6b9
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-06-05 18:46:38 +0530

    app.jsx file route pages

---
+++ FrontEnd/Pages/Success.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Success = () => {
+  return (
+    <>
+      <h1>Success full page </h1>
+    </>
+  )
+}
+
+export default Success
--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Toaster } from "react-hot-toast";
+import Home from "../Pages/Home";
+import NotFound from "../Pages/NotFound";
+import Success from "../Pages/Success";
 
 const App = () => {
   return <Router>
-    <Route path="/" element={} />
-    <Route path="/" element={} />
-    <Route path="/" element={} />
-    <Route path="/" element={} />
+    <Route path="/" element={<Home/>} />
+    <Route path="/" element={<NotFound/>} />
+    <Route path="/" element={<Success/>} />
   </Router>
 }
 
