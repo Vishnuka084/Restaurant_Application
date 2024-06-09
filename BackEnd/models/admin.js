1acb483b3454fbf8a83baf697be5dafedeb25445
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-06-10 00:32:04 +0530

    Updated

---
+++ BackEnd/models/admin.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcryptjs';
+
+const adminSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: true,
+    match: [
+      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+      'Please provide a valid email',
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 6,
+    select: false,
+  },
+}, { timestamps: true });
+
+adminSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next(); 
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+export default Admin;
--- BackEnd/routes/reservationRoute.js
+++ BackEnd/routes/reservationRoute.js
@@ -1,7 +1,12 @@
 import express from "express";
 import { send_reservation,get_all_reservations, update_reservation, delete_reservation }  from "../controller/reservation.js";
+import { admin_login } from "../controller/admin.js";
+import { isAuthenticatedAdmin } from "../middlewares/auth.js";
 
 const router = express.Router();
+
+// Admin login ---->
+router.post("/admin/login", admin_login);
 
 // save reservation ------->
 router.post("/send", send_reservation);
@@ -9,10 +14,26 @@ router.post("/send", send_reservation);
 // list all reservation ----->
 router.get("/list", get_all_reservations);
 
-// reservation Updated ----------->
+// Updated reservation ----------->
 router.put("/update/:id", update_reservation);
 
-// reservation delete ----------->
+// delete reservation ----------->
 router.delete("/delete/:id" , delete_reservation);
+
+
+// Protect routes that require admin authentication
+router.use(isAuthenticatedAdmin);
+
+// Save reservation (protected)
+router.post("/send", isAuthenticatedAdmin, send_reservation);
+
+// List all reservations (protected)
+router.get("/list", isAuthenticatedAdmin, get_all_reservations);
+
+// Update reservation (protected)
+router.put("/update/:id", isAuthenticatedAdmin, update_reservation);
+
+// Delete reservation (protected)
+router.delete("/delete/:id", isAuthenticatedAdmin, delete_reservation);
 
 export default router;
